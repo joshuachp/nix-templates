f1fd6b4db0fc313d1a8dd9e5ea901edd0e3def33
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-05-30 15:24:33 +0200

    feat(go): improve go template

--- go/flake.nix
+++ go/flake.nix
@@ -1,50 +1,32 @@
 {
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
   outputs =
     { self
     , nixpkgs
     , flake-utils
-    , lib
-    , ...
     }:
+    flake-utils.lib.eachDefaultSystem (system:
     let
-      eachDefaultSystemMap = flake-utils.lib.eachDefaultSystemMap;
+      pkgs = import nixpkgs { inherit system; };
+      lib = pkgs.lib;
     in
-    rec {
-      packages = eachDefaultSystemMap (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          default = pkgs.buildGoModule {
-            src = "./";
-          };
-        });
-      apps = eachDefaultSystemMap (system: {
-        default = flake-utils.lib.mkApp {
-          drv = packages.${system}.default;
-        };
-      });
-      devShells = eachDefaultSystemMap (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              go
-              pre-commit
-            ];
-          };
-        });
-    };
+    {
+      packages.default = pkgs.buildGoModule {
+        pname = "hello";
+        version = "0.0.0";
+        src = "./";
+        vendorSha256 = lib.fakeSha256;
+      };
+      apps. default = flake-utils.lib.mkApp {
+        drv = self.packages.${system}.default;
+      };
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [
+          self.packages.${system}.default
+        ];
+      };
+    });
 }
