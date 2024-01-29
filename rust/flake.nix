92b07d8a8646e9e0fffc21784ceba914fff65175
Joshua Chapman <joshua.chapman@joshuachp.dev>
2024-01-29 11:38:33 +0100

    refactor(rust): use crane with rust-overlay

--- rust/flake.nix
+++ rust/flake.nix
@@ -2,49 +2,64 @@
   description = "A rust example flake";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     crane = {
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
     };
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
     { self
     , nixpkgs
-    , fenix
+    , rust-overlay
     , crane
     , flake-utils
+    , ...
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { inherit system; };
-      toolchain = fenix.packages.${system}.stable;
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (import rust-overlay) ];
+      };
+      toolchain = pkgs.rust-bin.stable.latest.default;
       craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+      packages = self.packages.${system};
+      inherit (pkgs) callPackage;
     in
     {
       packages.default = craneLib.buildPackage {
         src = craneLib.cleanCargoSource (craneLib.path ./.);
-
         srictDeps = true;
       };
 
       apps.default = flake-utils.lib.mkApp {
-        drv = self.packages.${system}.default;
+        drv = packages.default;
       };
 
-      devShells.default = pkgs.mkShell {
-        inputsFrom = with self.packages.${system}; [
-          default
-        ];
-        packages = with pkgs; [
-          pre-commit
-          nixpkgs-fmt
-          rust-analyzer
-        ];
-      };
+      devShells.default =
+        let
+          toolchainDev = (toolchain.override {
+            extensions = [ "rust-analyzer" "rust-src" ];
+          });
+        in
+        pkgs.mkShell {
+          inputsFrom = [
+            packages.default
+          ];
+          packages = with pkgs; [
+            pre-commit
+            nixpkgs-fmt
+            toolchainDev
+          ];
+          RUST_SRC_PATH = "${toolchainDev}";
+        };
     });
 }
